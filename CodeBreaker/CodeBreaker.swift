571dc69cdc727076a4938e77bebb35bbdfbda73e
6510615245 <6510615245@student.tu.ac.th>
2026-01-21 15:41:35 +0700

    part2

---
+++ CodeBreaker/CodeBreaker.swift
@@ -0,0 +1,18 @@
+//
+//  CodeBreaker.swift
+//  CodeBreaker
+//
+//  Created by นางสาวพลอยพรรณ เต็งประยูร on 21/1/2569 BE.
+//
+
+import SwiftUI
+
+struct CodeBreaker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CodeBreaker()
+}
